318a6cf517f3e6aa6283e993d4d4e7200e9d82dc
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-02-15 20:04:02 -0600

    Formatting and stuff

--- src/scenario/scenario_name.cpp
+++ src/scenario/scenario_name.cpp
@@ -1,46 +1,39 @@
-#include <SMS/manager/FlagManager.hxx>
+#include <SMS/Manager/FlagManager.hxx>
 #include <SMS/System/MarDirector.hxx>
 #include <SMS/raw_fn.hxx>
 
-#include "sdk.h"
-#include "BetterSMS/logging.hxx"
+#include <BetterSMS/logging.hxx>
+#include <BetterSMS/module.hxx>
 
-
-char *loadStageNameFromBMG(void *unk, u32 index)
-{
-    void *mapBMG = JKRFileLoader::getGlbResource("/scene/map/stagename.bmg");
+char *loadStageNameFromBMG(void *global_bmg, u32 index) {
+    void *stage_bmg = JKRFileLoader::getGlbResource("/scene/map/stagename.bmg");
     char *message;
-    if (mapBMG)
-    {
-        BetterSMS::Console::log("[WARNING] /scene/map/stagename.bmg missing from archive, falling back to default stage name loading\n");
-        message = (char *)SMSGetMessageData__FPvUl(mapBMG, 0);
-    }
-    else
-    {
-        message = (char *)SMSGetMessageData__FPvUl(unk, index);
+    if (stage_bmg) {
+        BetterSMS::Console::log("[WARNING] /scene/map/stagename.bmg missing from archive, falling "
+                                "back to default stage name loading\n");
+        message = (char *)SMSGetMessageData__FPvUl(stage_bmg, 0);
+    } else {
+        message = (char *)SMSGetMessageData__FPvUl(global_bmg, index);
     }
     return message;
 }
 
-char *loadScenarioNameFromBMG(void *unk, u32 index)
-{
-    void *mapBMG = JKRFileLoader::getGlbResource("/scene/map/stagename.bmg");
+char *loadScenarioNameFromBMG(void *global_bmg, u32 index) {
+    void *stage_bmg = JKRFileLoader::getGlbResource("/scene/map/stagename.bmg");
 
     char *message;
-    if (mapBMG)
-    {
-        message = (char *)SMSGetMessageData__FPvUl(mapBMG, 1);
-    }
-    else
-    {
-        BetterSMS::Console::log("[WARNING] /scene/map/stagename.bmg missing from archive, falling back to default scenario name loading\n");
-        message = (char *)SMSGetMessageData__FPvUl(unk, index);
+    if (stage_bmg) {
+        message = (char *)SMSGetMessageData__FPvUl(stage_bmg, 1);
+    } else {
+        BetterSMS::Console::log("[WARNING] /scene/map/stagename.bmg missing from archive, falling "
+                                "back to default scenario name loading\n");
+        message = (char *)SMSGetMessageData__FPvUl(global_bmg, index);
     }
     return message;
 }
 
 SMS_PATCH_BL(SMS_PORT_REGION(0x80172704, 0x802A0C00, 0, 0), loadStageNameFromBMG);
-SMS_PATCH_BL(SMS_PORT_REGION(0x801727a0, 0x802A0C00, 0, 0), loadScenarioNameFromBMG);
+SMS_PATCH_BL(SMS_PORT_REGION(0x801727A0, 0x802A0C00, 0, 0), loadScenarioNameFromBMG);
 
 SMS_PATCH_BL(SMS_PORT_REGION(0x80156d2c, 0x802A0C00, 0, 0), loadStageNameFromBMG);
-SMS_PATCH_BL(SMS_PORT_REGION(0x80156e04, 0x802A0C00, 0, 0), loadScenarioNameFromBMG);
+SMS_PATCH_BL(SMS_PORT_REGION(0x80156E04, 0x802A0C00, 0, 0), loadScenarioNameFromBMG);
